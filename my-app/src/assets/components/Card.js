78a9ff2aacc7a5dc72d5e3d19fc4e9f38ab8da6d
Edi <ediprekpalaj78@gmail.com>
2023-03-28 23:46:51 -0400

    Fix features that were causing my flipping and disabling cards to go haywire.

--- my-app/src/assets/components/Card.js
+++ my-app/src/assets/components/Card.js
@@ -3,12 +3,12 @@ import "../css/Card.css"
 import backPNG from "../img/back.png";
 
 
-export default function Card({ card, clickCallee, flipped }) {
+export default function Card({ card, clickCallee, flipped, disabled }) {
     function clickCaller() {
         clickCallee(card)
     }
     return (
-        <div className="card" key={card.id}>
+        <div className={`card ${card.matched ? 'flipped' : ''}`} key={card.id}>
             <div className={flipped ? "flipped" : ""}>
                 <div>
                     <img className="frontOfCard" src={card.src} alt="front"/>
@@ -18,3 +18,4 @@ export default function Card({ card, clickCallee, flipped }) {
         </div>
     );
 }
+  
